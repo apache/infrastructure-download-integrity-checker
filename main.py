2b282be6dadbc94275db56aab247b6c6c8d35e00
Sebb <sebb@apache.org>
2022-07-22 15:49:28 +0100

    Allow for missing key_id

--- main.py
+++ main.py
@@ -216,7 +216,8 @@ def verify_files(project: str, keychain: gnupg.GPG, is_podling: bool) -> dict:
                     verified = keychain.verify_file(open(asc_filepath, "rb"), data_filename=filepath)
                     if not verified.valid:
                         if verified.key_id not in known_fingerprints:
-                            if "gpg-exit" in verified.key_id:  # HACK: gpg-exit key ID means gpg crashed.
+                            # key_id can be None e.g. if sig is garbage
+                            if not verified.key_id or "gpg-exit" in verified.key_id:  # HACK: gpg-exit key ID means gpg crashed.
                                 push_error(errors, filepath, f"[CHK05] The signature file {filename}.asc could not be used to verify the release artifact (corrupt sig?)")
                             else:
                                 push_error(errors, filepath, f"[CHK01] The signature file {filename}.asc was signed with a key not found in the project's KEYS file: {verified.key_id}")
